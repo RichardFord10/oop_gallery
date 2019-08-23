cb90bcdfb16b9887fb59083cd51b6f689e93d987
richardford10 <richardford10@gmail.com>
2019-08-22 23:21:59 -0400

    First commit

---
+++ OOP/gallery/admin/includes/functions.php
@@ -0,0 +1,25 @@
+<?php
+
+function class_auto_loader($class){
+  
+  
+  $class = strtolower($class);
+  
+  $the_path = "includes/{$class}.php";
+  
+  if(is_file($the_path) && !class_exists($class)) {
+    
+    include $the_path;
+    
+    
+  }
+  
+
+}
+
+spl_autoload_register('class_auto_loader');
+
+
+
+
+?>
